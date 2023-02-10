14d15a72c3dbe8b9a4a4eb648c1aae4f0531bdaa
Angel Ruiz <ruizca@noa.gr>
2023-02-10 14:47:13 +0200

    fix installation problems

--- setup.py
+++ setup.py
@@ -20,9 +20,20 @@ class CustomInstall(install, object):
 setup(
     name="msvst",
     version="1.0",
-    description="lol",
+    license='CeCILL',
+    description='Multi-Scale Variance Stabilization Transform',
+    author='Angel Ruiz',
+    author_email='angel.ruizca@gmail.com',
+    url="https://github.com/ruizca/msvst",
     packages=["msvst"],
     package_dir={"msvst": "msvst/"},
     package_data={"msvst": ["bin/*"]},
+    include_package_data=True,
     cmdclass={"install": CustomInstall},
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
